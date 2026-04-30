a91c0cefb6c651f40b3ce45d5b3c1d305b4e5077
qazaleh <qkkalhori@gmail.com>
2026-04-30 15:26:33 +0200

    Added external payment nodes
    - Adds nodes for transaction not in the graph but for which we have an outpoint

--- src/graph/builder.rs
+++ src/graph/builder.rs
@@ -1,12 +1,15 @@
 use bdk_chain::TxGraph;
 use bdk_wallet::Wallet;
-use bitcoin::{Txid, hashes::Hash};
+use bitcoin::{OutPoint, Txid, hashes::Hash};
+use std::collections::HashSet;
 use crate::graph::view::{TxGraphView, TxNodeView, TxEdgeView};
 
 pub fn build_view(tx_graph: &TxGraph, wallet: &Wallet) -> TxGraphView {
     let mut nodes = Vec::new(); 
     let mut edges = Vec::new();
     let full_txs_vec= tx_graph.full_txs().collect::<Vec<_>>();
+    let all_tx_outptuts: HashSet<OutPoint> = tx_graph.all_txouts().enumerate().map(|(_, op)| op.0).collect();
+    let mut visited_tx_outputs: HashSet<OutPoint> = HashSet::new();
 
     for tx_node in full_txs_vec.iter() {
         nodes.push(TxNodeView{
@@ -33,6 +36,7 @@ pub fn build_view(tx_graph: &TxGraph, wallet: &Wallet) -> TxGraphView {
                         0
                     }
             });
+            visited_tx_outputs.insert(input.previous_output);
         }
     }
     // Adding the unspent outputs to the graph
@@ -42,6 +46,20 @@ pub fn build_view(tx_graph: &TxGraph, wallet: &Wallet) -> TxGraphView {
             to: unspent_output.outpoint.txid,
             vout: unspent_output.outpoint.vout,
             amnt: unspent_output.txout.value.to_sat()
+        });
+        visited_tx_outputs.insert(unspent_output.outpoint);
+    }
+    for (_, external_paid_outputs) in all_tx_outptuts.difference(&visited_tx_outputs).enumerate(){
+         nodes.push(TxNodeView{
+            txid: Txid::all_zeros(),
+            is_confirmed: false, 
+            external: true,
+        });       
+        edges.push(TxEdgeView{
+            from: external_paid_outputs.txid,
+            to: Txid::all_zeros(),
+            vout: external_paid_outputs.vout,
+            amnt: tx_graph.get_txout(*external_paid_outputs).unwrap().value.to_sat()
         });
     }
 
