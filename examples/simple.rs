80f846126d654c541ff956b711c48d5ec2f12834
qazaleh <qkkalhori@gmail.com>
2026-05-04 18:51:59 +0200

    Incosistent graph example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,37 +1,48 @@
+use std::str::FromStr;
+use std::sync::Arc;
+
+use bdk_chain::{BlockId, CanonicalizationParams, ConfirmationBlockTime};
+use bdk_wallet::coin_selection;
+use bdk_wallet::descriptor::{calc_checksum, DescriptorError};
+use bdk_wallet::error::CreateTxError;
+use bdk_wallet::psbt::PsbtUtils;
+use bdk_wallet::signer::{SignOptions, SignerError};
 use bdk_wallet::KeychainKind;
-use bitcoin::{Amount, OutPoint, Transaction, TxIn, TxOut, absolute, transaction};
+use bdk_wallet::{AddressInfo, Balance, PersistedWallet, Update, Wallet, WalletTx};
+use bitcoin::constants::COINBASE_MATURITY;
+use bitcoin::hashes::Hash;
+use bitcoin::script::PushBytesBuf;
+use bitcoin::sighash::{EcdsaSighashType, TapSighashType};
+use bitcoin::taproot::TapNodeHash;
+use bitcoin::{
+    absolute, transaction, Address, Amount, BlockHash, FeeRate, Network, OutPoint, ScriptBuf,
+    Sequence, SignedAmount, Transaction, TxIn, TxOut, Txid,
+};
+use rand::rngs::StdRng;
+use rand::SeedableRng;
+
 use std::fs;
 use tx_graph_visualizer::graph::build_view;
 use tx_graph_visualizer::test_utils::*;
 
 #[allow(clippy::print_stdout)]
 fn main() {
-    let (mut wallet, _) = get_funded_wallet_wpkh();
-    let outpoint = wallet.list_unspent().next().unwrap().outpoint;
+     let (mut wallet, _) = get_funded_wallet_wpkh();
     let external_tx = Transaction {
         input: vec![TxIn {
             previous_output: OutPoint {
-                txid: outpoint.txid, // not owned by wallet
-                vout: outpoint.vout,
+                txid: Txid::all_zeros(), // not owned by wallet
+                vout: 0,
             },
             ..Default::default()
         }],
-        output: vec![
-            TxOut {
-                value: Amount::from_sat(40_000),
-                script_pubkey: wallet
-                    .next_unused_address(KeychainKind::Internal)
-                    .address
-                    .script_pubkey(),
-            },
-            TxOut {
-                value: Amount::from_sat(5_000),
-                script_pubkey: wallet
-                    .next_unused_address(KeychainKind::Internal)
-                    .address
-                    .script_pubkey(),
-            },
-        ],
+        output: vec![TxOut {
+            value: Amount::from_sat(50_000),
+            script_pubkey: wallet
+                .next_unused_address(KeychainKind::External)
+                .address
+                .script_pubkey()
+        }],
         version: transaction::Version::ONE,
         lock_time: absolute::LockTime::ZERO,
     };
@@ -41,8 +52,8 @@ fn main() {
     let tx_spend_1 = Transaction {
         input: vec![TxIn {
             previous_output: OutPoint {
-                txid: external_txid, // not owned by wallet
-                vout: 1,
+                txid: external_txid , // not owned by wallet
+                vout: 0,
             },
             ..Default::default()
         }],
@@ -51,7 +62,7 @@ fn main() {
             script_pubkey: wallet
                 .next_unused_address(KeychainKind::Internal)
                 .address
-                .script_pubkey(),
+                .script_pubkey()
         }],
         version: transaction::Version::ONE,
         lock_time: absolute::LockTime::ZERO,
@@ -68,7 +79,7 @@ fn main() {
             ..Default::default()
         }],
         output: vec![TxOut {
-            value: Amount::from_sat(35_000),
+            value: Amount::from_sat(45_000),
             script_pubkey: wallet
                 .next_unused_address(KeychainKind::Internal)
                 .address
@@ -79,6 +90,8 @@ fn main() {
     };
 
     insert_tx(&mut wallet, tx_spend);
+    let wallet_balance = wallet.balance();
+    println!("the wallet balance is {} ", wallet_balance);
 
     let tx_graph_vis = build_view(&wallet.tx_graph(), &wallet).to_dot();
     fs::write("graph.dot", tx_graph_vis).unwrap();
